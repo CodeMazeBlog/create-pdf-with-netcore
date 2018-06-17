548e19c2cd729826e5c227e69f44dcc373745b53
Marinko <marinkospasojevic@gmail.com>
2018-06-17 22:28:44 +0200

    Create PDF with .NET Core Web API end

---
+++ PDF_Generator/PDF_Generator/Utility/TemplateGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace PDF_Generator.Utility
+{
+    public static class TemplateGenerator
+    {
+        public static string GetHTMLString()
+        {
+            var employees = DataStorage.GetAllEmployess();
+
+            var sb = new StringBuilder();
+            sb.Append(@"
+                        <html>
+                            <head>
+                            </head>
+                            <body>
+                                <div class='header'><h1>This is the generated PDF report!!!</h1></div>
+                                <table align='center'>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>LastName</th>
+                                        <th>Age</th>
+                                        <th>Gender</th>
+                                    </tr>");
+
+            foreach (var emp in employees)
+            {
+                sb.AppendFormat(@"<tr>
+                                    <td>{0}</td>
+                                    <td>{1}</td>
+                                    <td>{2}</td>
+                                    <td>{3}</td>
+                                  </tr>", emp.Name, emp.LastName, emp.Age, emp.Gender);
+            }
+
+            sb.Append(@"
+                                </table>
+                            </body>
+                        </html>");
+
+            return sb.ToString();
+        }
+    }
+}
