bc900d6e6adaa8751becc7ec2fe9606af77e6ebf
Marinko <marinkospasojevic@gmail.com>
2019-02-20 19:10:31 +0100

    Added support for .NET Core 2.2

---
+++ PDF_Generator/PDF_Generator/Controllers/PdfCreatorController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using PDF_Generator.Utility;
+
+namespace PDF_Generator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfCreatorController : ControllerBase
+    {
+        private IConverter _converter;
+
+        public PdfCreatorController(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        [HttpGet]
+        public IActionResult CreatePDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report",
+                //Out = @"D:\PDFCreator\Employee_Report.pdf"  USE THIS PROPERTY TO SAVE PDF TO A PROVIDED LOCATION
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetHTMLString(),
+                //Page = "https://code-maze.com/", USE THIS PROPERTY TO GENERATE PDF CONTENT FROM AN HTML PAGE
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            //_converter.Convert(pdf); IF WE USE Out PROPERTY IN THE GlobalSettings CLASS, THIS IS ENOUGH FOR CONVERSION
+
+            var file = _converter.Convert(pdf);
+
+            //return Ok("Successfully created PDF document.");
+            //return File(file, "application/pdf", "EmployeeReport.pdf"); //USE THIS RETURN STATEMENT TO DOWNLOAD GENERATED PDF DOCUMENT
+            return File(file, "application/pdf");
+
+        }
+    }
+}
---
+++ PDF_Generator/PDF_Generator/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace PDF_Generator.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
